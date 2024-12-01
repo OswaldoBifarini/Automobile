a1a05743ccd1497e831d92cd6160558c703270a8
WilliAn-333 <www.pegazo23@gmail.com>
2024-11-30 19:58:10 -0500

    otro cambio inesperafo

--- src/main/java/ec/edu/espoc/automovil/TipoCombustible.java
+++ src/main/java/ec/edu/espoc/automovil/TipoCombustible.java
@@ -1,18 +1,11 @@
 package ec.edu.espoc.automovil;
 
 public enum TipoCombustible {
-<<<<<<< HEAD
-    GASOLINA,
-    DIESEL,
-    BIOETANOL,
-    BIODIESEL,
-    GAS_NATURAL
-=======
+
     GASOLINE, 
     BIOETHANOL, 
     DIESEL, 
     BIODIESEL, 
     NATURALGAS
     
->>>>>>> dev-willian
 }
