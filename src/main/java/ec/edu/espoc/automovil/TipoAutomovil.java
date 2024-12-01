d99c378b74a80c88579f88176187a100a9cde78f
WilliAn-333 <www.pegazo23@gmail.com>
2024-11-30 19:49:51 -0500

    willian

--- src/main/java/ec/edu/espoc/automovil/TipoAutomovil.java
+++ src/main/java/ec/edu/espoc/automovil/TipoAutomovil.java
@@ -1,20 +1,12 @@
 package ec.edu.espoc.automovil;
 
 public enum TipoAutomovil {
-<<<<<<< HEAD
-   
-    SUBCOMPACTO,
-    CIUDAD,
-    FAMILIAR,
-    COMPACTO,
-    EJECUTIVO,
-=======
+
     CITY, 
     SUBCOMPACT, 
     COMPACT, 
     FAMILY, 
     EXECUTIVE, 
->>>>>>> dev-willian
     SUV
     
 }
